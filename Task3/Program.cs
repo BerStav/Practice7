a20c35d5eb9f6b3db989b3682dd8eb05b7d6602d
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-19 00:49:27 +0700

    task3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив из целых чисел. Надите среднее арифметическое элементов в каждом столбце.
+
+int[,] twoDimArray = new int[5, 5];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
+
+for (int j = 0; j < twoDimArray.GetLength(1); j++)
+{
+    double avarage = 0;
+
+    for (int i = 0; i < twoDimArray.GetLength(0); i++)
+    {
+        avarage = (avarage + twoDimArray[i, j]);
+    }
+    avarage = avarage / 5;
+
+    if (j == 4)
+    {
+        Console.Write(avarage + ".");
+    }
+    else
+    {
+        Console.Write(avarage + "; ");
+    }
+}
