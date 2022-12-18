687e8c967945cc4d189f50bef91c09c99b1fddaa
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-18 21:19:58 +0700

    task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m x n, заполненный случайными вещественными числами.
+
+Console.Write("Please, enter the number of lines: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Please, enter the number of columns: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] twoDimArray = new double[rows, columns];
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
